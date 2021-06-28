390ae25fc76c7193430c44f234e480f2003cfd28
uytv-1398 <tran.van.uy@sun-asterisk.com>
2021-06-28 22:56:06 +0700

    Middleware

--- config/api.routes.js
+++ config/api.routes.js
@@ -9,12 +9,12 @@ var UsersController = require('../controllers/api/users_controller.js');
 var TodosController = require('../controllers/api/todos_controller.js');
 
 module.exports = function(router) {
-  router.get('/top_pages', AuthController.authenticate_user, TopPagesController.index);
+  router.get('/top_pages', AuthMiddleware.authenticate_user, TopPagesController.index);
   router.post('/register', UserMiddleware.validate_register, UsersController.register);
   router.post('/auth/sign_in', AuthController.sign_in);
   router.post('/auth/authenticate_user', AuthMiddleware.authenticate_user);
-  router.get('/auth/user', AuthController.authenticate_user, AuthController.user);
-  router.get('/todos', AuthController.authenticate_user, TodosController.index);
-  router.post('/todos', AuthController.authenticate_user, TodosController.create);
-  router.put('/todo/:id', AuthController.authenticate_user, TodosController.update);
+  router.get('/auth/user', AuthMiddleware.authenticate_user, AuthController.user);
+  router.get('/todos', AuthMiddleware.authenticate_user, TodosController.index);
+  router.post('/todos', AuthMiddleware.authenticate_user, TodosController.create);
+  router.put('/todo/:id', AuthMiddleware.authenticate_user, TodosController.update);
 }
